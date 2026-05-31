6ae35c60586c8dc9afe56973cc82e69cba91b823
GxpixamoDev <gxpixamodev@gmail.com>
2026-05-31 14:47:53 +0600

    Made  aCalculator

---
+++ projects/calculator/calculator.py
@@ -0,0 +1,21 @@
+num1 = input("Enter your 1st number: ")
+num2 = input("Enter your 2nd number: ")
+decision = input("Enter the operation you want to perform (option: add, subtract, multiply, divide): ")
+
+if decision == "add":
+    result = int(num1) + int(num2)
+
+elif decision == "subtract":
+    result = int(num1) - int(num2)
+
+elif decision == "multiply":
+    result = int(num1) * int(num2)
+
+elif decision == "divide":
+    result = int(num1) / int(num2)
+
+else:
+    print("Invalid operation")
+
+
+print("The result is: " + str(result))
